11459e13b2acd9e8b211e877690fe0c5900179a5
Markus Hoehn <markushoehn2007@gmail.com>
2024-04-13 00:58:04 -0500

    image stream

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask,render_template,Response
+from flask import Flask, render_template, request
+import numpy as np
 import cv2
+import io
+import base64
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def camera():
-    return render_template('camera.html')
+    if request.method == 'POST' and request.form.get("photo") != None:
+        base64_string = request.form.get("photo")
+
+        image_bytes = base64.b64decode(base64_string)
+        
+        in_memory_file = io.BytesIO(image_bytes)
+        data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
+        color_image_flag = 1
+        img = cv2.imdecode(data, color_image_flag)
+
+        cv2.imwrite("image.jpg", img)
+
+        return render_template('camera.html')
+    else:
+        return render_template('camera.html')

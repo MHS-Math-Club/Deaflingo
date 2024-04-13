cc851649acdd43692bc3f4e71fe9d36f9a963dd0
Markus Hoehn <markushoehn2007@gmail.com>
2024-04-13 14:43:04 -0500

    fixed color distortion

--- app.py
+++ app.py
@@ -35,15 +35,17 @@ def update():
     
     frame = np.array(raw_image)
     H, W, _ = frame.shape
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    results = hands.process(frame_rgb)
+    
+    # Convert the frame from RGB to BGR
+    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    results = hands.process(frame_bgr)
+    
     if results.multi_hand_landmarks:
-        print("landmarks")
         # Take the first detected hand
         hand_landmarks = results.multi_hand_landmarks[0]
 
         mp_drawing.draw_landmarks(
-            frame,  # image to draw
+            frame_bgr,  # image to draw
             hand_landmarks,  # model output
             mp_hands.HAND_CONNECTIONS,  # hand connections
             mp_drawing_styles.get_default_hand_landmarks_style(),
@@ -76,17 +78,16 @@ def update():
 
         predicted_character = labels_dict[int(prediction[0])]
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+        cv2.rectangle(frame_bgr, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame_bgr, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
-    else:
-        print("no")
 
     # Convert numpy array back to PIL image
-    processed_with_text = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    processed_with_text = Image.fromarray(cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB))
     
     # Save the processed image with text as "processed.jpg"
     processed_with_text.save("images/processed.jpg")
+
 
 @app.route('/', methods=['GET', 'POST'])    
 def camera():

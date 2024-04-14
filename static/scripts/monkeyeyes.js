c97e255997e3e22f58edd239757d023ff4867907
Andrei Mazin <andreimazin07@gmail.com>
2024-04-14 04:22:02 -0500

    cleanup

--- static/scripts/monkeyeyes.js
+++ static/scripts/monkeyeyes.js
@@ -1,15 +1,24 @@
+// Wait for the document to be fully loaded
 $(document).ready(function() {
+  // Function to rotate the green monkey eyes based on mouse movement
   $(".green_monkey_move-area").mousemove(function(event) {
-    var eye = $(".green_monkey_eye");
-    var x = (eye.offset().left) + (eye.width() / 2);
-    var y = (eye.offset().top) + (eye.height() / 2);
-    var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 45;
-    eye.css({
-      '-webkit-transform': 'rotate(' + rot + 'deg)',
-      '-moz-transform': 'rotate(' + rot + 'deg)',
-      '-ms-transform': 'rotate(' + rot + 'deg)',
-      'transform': 'rotate(' + rot + 'deg)'
-    });
+      // Select the green monkey eye element
+      var eye = $(".green_monkey_eye");
+      
+      // Calculate the center of the eye
+      var x = (eye.offset().left) + (eye.width() / 2);
+      var y = (eye.offset().top) + (eye.height() / 2);
+      
+      // Calculate the angle of rotation based on mouse position
+      var rad = Math.atan2(event.pageX - x, event.pageY - y);
+      var rot = (rad * (180 / Math.PI) * -1) + 45;
+      
+      // Apply the rotation to the eye
+      eye.css({
+          '-webkit-transform': 'rotate(' + rot + 'deg)',
+          '-moz-transform': 'rotate(' + rot + 'deg)',
+          '-ms-transform': 'rotate(' + rot + 'deg)',
+          'transform': 'rotate(' + rot + 'deg)'
+      });
   });
 });
